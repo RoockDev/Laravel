fd2cc7c4a4b95e81fa314d9540f016f189b5ca5b
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-01 18:28:23 +0200

    Ejercicio 1 Laravel Servicio con rutas

--- Ejercicio1/routes/api.php
+++ Ejercicio1/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ServicioController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('devolver-monedas/{cantidad}', [ServicioController::class,'devolverMonedas'])
+->where('cantidad', '[0-9]+(\.[0-9]{1,2})?'); //permite maximo 2 decimales 
+Route::get('calcular-edad/{fecha_nacimiento?}',[ServicioController::class, 'calcularEdad']);
+Route::get('comparar-numero/{num1}/{num2}',[ServicioController::class, 'compararNumeros'])
+->where('num1', '[0-9]+')
+->where('num2','[0-9]+'); //permite solo numeros enteros positivos
